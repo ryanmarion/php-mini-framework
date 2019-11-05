9d17075ff389f4fea431451fa671f145623505a8
Ryan Marion <ryan.marion@intimetec.com>
2019-11-05 16:08:33 -0700

    talking to db successfully

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -2,11 +2,7 @@
 
 namespace App\Controllers;
 
-use PDO;
-
 class HomeController {
-    public function __construct(PDO $db){
-    }
 
     public function index($response){
         return $response->setBody('check');
